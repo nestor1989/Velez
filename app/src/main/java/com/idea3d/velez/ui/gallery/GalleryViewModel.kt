bf1277fce03441a38871f6b9ae6ea3016a34d81d
Néstor del Río <nestordelrio1989@gmail.com>
2022-03-05 20:12:13 -0300

    initial configs

---
+++ app/src/main/java/com/idea3d/velez/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.idea3d.velez.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/idea3d/velez/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.idea3d.velez.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
